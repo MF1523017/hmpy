62ad7f1aec37465506b0cbc4b6c8739ed77321e2
MF1523017 <554896649@qq.com>
2016-10-13 20:50:05 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,15 @@
+#homework.py
+##用于批量批改c++ 作业
+##usage:
+>python homeworkDir testFilePath studentsNameFile
+
+* homeworkDir:所有同学作业所在目录
+* testFilePath:测试文件所在路径
+* studentsNameFile: 学生学号名单
+	* eg:
+		* 0000
+		* 1111
+		*  ......		
+
+##result:
+* time_result.csv
--- homework.py
+++ homework.py
@@ -13,8 +13,11 @@ import zipfile
 import shutil 
 import re
 import pdb
-import copy
 from collections import OrderedDict
+import time
+def getTime():        
+    return time.strftime('%Y_%m_%d_%H_%M_%S',time.localtime())
+
 class Homework:
     def __init__(self,path,testFile,result,zipFiles=None,zipDirs=None):
         self.path=path
@@ -47,41 +50,54 @@ class Homework:
         #print flist
         tmps=self.nums(flist[-1])
       #  pdb.set_trace()
-        print tmps
-        self.result[tmps]=0;
-        return os.path.join(flist[0],tmps)
+        #print tmps
+        self.result[tmps]=0;#初始化成绩
+        return os.path.join(flist[0],tmps)#返回绝对路径
             
     def unzip_dir(self):
         self.__zipfiles()
-        print self.zipFiles
+       # print self.zipFiles
         for zipF in self.zipFiles:
             numpath=self.extractNum(zipF[:-4])
             self.zipDirs.append(numpath)
             if not os.path.exists(numpath):
                 os.mkdir(numpath)
+           # print numpath
+            try:
+                srcZip=zipfile.ZipFile(zipF,'r')
                 
-            srcZip=zipfile.ZipFile(zipF,'r')
+            except:
+                print zipF
+                continue
             for eachFile in srcZip.namelist():
                 if type(eachFile) is unicode:
+                   # pdb.set_trace()
                     eachFileCopy=eachFile.encode('utf8')
                 else:
                     eachFileCopy=eachFile
                 eachFileName=os.path.normpath(os.path.join(numpath,eachFileCopy))
+                eachFileName=eachFileName.replace(' ','')
                 eachDirName=os.path.dirname(eachFileName)
+                eachDirName=eachDirName.replace(' ','')
                 if not os.path.exists(eachDirName):
                     try:
                         os.mkdir(eachDirName)
                     except:
                         pass
                 if eachFileName.endswith('.h')or eachFileName.endswith('.cpp'):
-                   
                     fd=open(eachFileName,'w')
                     fd.write(srcZip.read(eachFile))
                     fd.close()
                     try:
+                        if self.checkMain(eachFileName):
+                            os.remove(eachFileName)
+                            continue
+                        #pdb.set_trace()
+                        self.checkStdafx(eachFileName)
                         shutil.copy(eachFileName,numpath)
                     except:
                         pass
+            shutil.copy(self.testFile,numpath)
             srcZip.close()
     def checkMain(self,f):
         with open(f,'r') as cpp:
@@ -91,45 +107,39 @@ class Homework:
         return False
     def checkStdafx(self,f):
         tmp=list()
-        io=0
         with open(f,'r') as cpp:
             for line  in cpp:
-                if "stdafx.h" in line or 'targetver.h' in line or 'pragma' in line:
+                if "stdafx.h" in line:
+                   # pdb.set_trace()
                     continue
                 elif 'main' in line:
-                    tmp.append('int main(int argc,char ** argv)\n')
+                    if '{' in line:
+                        tmp.append('int main(int argc,char ** argv){\n')
+                    else:
+                        tmp.append('int main(int argc,char ** argv)\n')
                 elif 'iostream' in line:
-                    io+=1
-                    tmp.append(line)
+                    continue
                 else:
                     tmp.append(line)
         with open(f,'w') as ncpp:
-            if io==1:
-                pass
-            else:
-                ncpp.write('#include<iostream>\n')
+         #   pdb.set_trace()
+            ncpp.write('#include<iostream>\n')
             for nline in tmp:
                 ncpp.write(nline)
                 
     def compileCpp(self):
         for path in self.zipDirs:
            # pdb.set_trace()
-            #print path
-            if 'stdafx.h' in os.listdir(path):
-                shutil.copy(self.testFile,path)
-            else:
-                for f in os.listdir(path):
-                    if f.endswith('.cpp') and not self.checkMain(os.path.normpath(os.path.join(path,f))):
-                        self.checkStdafx(os.path.normpath(os.path.join(path,f)))
-                        shutil.copy(self.testFile,path)
-                    elif f.endswith('.h') or f.endswith('.cpp'):
-                        self.checkStdafx(os.path.normpath(os.path.join(path,f)))
-           # pdb.set_trace()
-            os.system('g++ -std=c++11 -o {0}/test1 {0}/*.h {0}/*.cpp'.format(path))
-            os.system('g++ -o {0}/test1 {0}/*.h {0}/*.cpp'.format(path))
+            os.system('g++ -std=c++11 -o {0}/test11 {0}/*.h {0}/*.cpp '.format(path))
+            os.system('g++ -o {0}/test1 {0}/*.h {0}/*.cpp '.format(path))
             key=list(os.path.split(path))[-1]
+            if os.path.exists('{0}/test.result'.format(path)):#remove old test.result file
+                os.remove('{0}/test.result'.format(path))
             if os.path.exists('{0}/test1'.format(path)):
                 os.system('{0}/test1 > {0}/test.result'.format(path))
+                self.result[key]=self.computeResult('{0}/test.result'.format(path))
+            if os.path.exists('{0}/test11'.format(path)):
+                os.system('{0}/test11 >> {0}/test.result'.format(path))
                 self.result[key]=self.computeResult('{0}/test.result'.format(path))
             else:
                 self.result[key]=60
@@ -150,7 +160,7 @@ class Homework:
         else:
             return max(result)
     def save(self):
-        with open('result.csv','w') as r:
+        with open('{}result.csv'.format(getTime()),'w') as r:
             for key,value in self.result.items():
                 r.write(str(key))
                 r.write(',')
@@ -159,47 +169,77 @@ class Homework:
 class RarHomework(Homework):
     def __init__(self,path,testFile,result,zipFiles=None,zipDirs=None):
         Homework.__init__(self,path,testFile,result)
+        self.rarFile=list()
     def __rarfiles(self):
         dirs=os.listdir(self.path)
         for d in dirs:
             if d.endswith('.rar'):
-                self.zipFiles.append(os.path.abspath(d))   
+                self.rarFile.append(os.path.abspath(d))   
     def filesOrDir(self,l):
         for f in l:
             if '.h' in f or '.cpp' in f:
                 return True
         return False
     def hcppFile(self,p):
-        if os.path.isfile(p) and (p.endswith('.h') or p.endswith('.cpp')):
-            self.hcpp.append(os.path.abspath(p))
-            return 0
-        else:
-            
-            for i in os.listdir(p):
-                self.hcppFile(os.path.abspath(i))
-        return 0
+        for lists in os.listdir(p): 
+            path = os.path.join(p, lists)
+            if path.endswith('.cpp') or path.endswith('.h'):
+                self.hcpp.append(path) 
+            if os.path.isdir(path): 
+                self.hcppFile(path) 
+    def checkSpace(self,tmps):
+        if ' ' in tmps:
+            tmps=tmps.replace(' ','')
+        return tmps
+    def copyHCpp(self,path):
+        if not self.hcpp:
+            return
+        for f in self.hcpp:
+            if self.checkMain(f):
+                os.remove(f)
+                continue
+            self.checkStdafx(f)
+            fs=os.path.split(f)[-1]  
+            shutil.move(f,os.path.join(path,fs))
     def unrar(self):
         self.__rarfiles()
-        print self.zipFiles
-        for rar in self.zipFiles:
+       # print self.zipFiles
+        print len(self.zipDirs)
+        for rar in self.rarFile:
             self.hcpp=list()
-            s1=os.system('rar x {}'.format(rar))
-            if s1>0:
-                continue
+            
             numpath=self.extractNum(rar[:-4])
-            self.zipDirs.append(numpath)
+            numpath=self.checkSpace(numpath)
             if not os.path.exists(numpath):
                 os.mkdir(numpath)
+            self.zipDirs.append(numpath)
+           # shutil.copy(rar,numpath)
+#            rarFile=os.path.split(rar)[-1]
+#            noSpaceRarFile=self.checkSpace(rarFile)
+            newRar=os.path.join(numpath,os.path.split(numpath)[-1]+'.rar')
             
-            if self.filesOrDir(os.listdir(self.path)):
-                os.system('mv *.h *.cpp {}'.format(numpath))
-            else:
+            shutil.copy(rar,newRar)
+            os.chdir(numpath)
+            s1=os.system('rar x {}'.format(newRar))
+            if s1>0:
+                print rar
                 continue
-                
-           
             
-           # shutil.copy(self.testFile,numpath)
+            self.hcppFile(numpath)
+            #print self.hcpp
+            #pdb.set_trace()
+            self.copyHCpp(numpath)
             
+            shutil.copy(self.testFile,numpath)
+    def combine(self):
+        self.unzip_dir()
+        self.unrar()
+        
+       # self.zipDirs.excend(self.rar)
+            
+            
+            
+
             
             
         
@@ -207,14 +247,20 @@ class RarHomework(Homework):
             
         
 if __name__=='__main__':
-    path=r'.'    
-    testFile=r'/home/lipei/cpp/hwpy/LibArray.cpp'
+    import sys
+    if len(sys.argv)!=4:
+        print 'usage: python homework.py homeworkDir testFilePath studentsNameFile'
+        exit()
+    path=sys.argv[1]    
+    testFile=sys.argv[2]
+    students=sys.argv[3]
     import numpy as np
-    students='students.txt'
     s=np.loadtxt(students,'int')
     r=RarHomework(path,testFile,s)
     r.unzip_dir()
     r.unrar()
+    print len(r.zipDirs)
+   # pdb.set_trace()
     r.compileCpp()
     r.save()
     
